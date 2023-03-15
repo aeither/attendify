7eb812e3c4022004ef4c67ec958a894bb3357981
aeither <36173828+aeither@users.noreply.github.com>
2023-03-15 15:19:51 +0100

    add polybase auth

---
+++ libs/hooks/use-polybase.ts
@@ -0,0 +1,102 @@
+import { Button, Stack, Typography } from "@mui/material";
+import { toast } from "sonner";
+import { Auth } from "@polybase/auth";
+import {
+  ethPersonalSign,
+  ethPersonalSignRecoverPublicKey,
+} from "@polybase/eth";
+import { Polybase } from "@polybase/client";
+import { useCollection } from "@polybase/react";
+import { useEffect, useState } from "react";
+
+const db = new Polybase({
+  defaultNamespace:
+    "pk/0xa6ca155c486fde1b93ec326c8d6d625a24cea697027449ea8a51dad1752e26998fd99b0ff13d7d3078fe5154a819868d171a2317f121b7328e7d15ca674ae3b3/alpha",
+});
+const auth = typeof window !== "undefined" ? new Auth() : null;
+
+async function getPublicKey() {
+  if (!auth) return;
+  const msg = "Login to app";
+  const sig = await auth.ethPersonalSign(msg);
+  const publicKey = ethPersonalSignRecoverPublicKey(sig, msg);
+  return "0x" + publicKey.slice(4);
+}
+
+export const useCollectionUser = () => {
+  const query = db.collection("User");
+  return useCollection(query);
+};
+
+export function usePB() {
+  const [authed, setAuthed] = useState(false);
+
+  const signIn = async () => {
+    if (!auth) return;
+
+    const res = await auth.signIn();
+    console.log("🚀 ~ file: use-polybase.ts:38 ~ signIn ~ res:", res);
+    if (!res) return;
+
+    // get public
+    let publicKey = res.publicKey ? res.publicKey : await getPublicKey();
+    if (!publicKey) return;
+
+    db.signer(async (data: string) => {
+      return {
+        h: "eth-personal-sign",
+        sig: await auth.ethPersonalSign(data),
+      };
+    });
+
+    // Create user if not exists
+    try {
+      const user = await db.collection("User").record(publicKey).get();
+      console.log("User", user);
+    } catch (e) {
+      await db.collection("User").create([publicKey]);
+    }
+
+    setAuthed(!!res);
+  };
+
+  const signOut = async () => {
+    if (!auth) return;
+    const res = await auth.signOut();
+    setAuthed(!!res);
+    toast("hello");
+  };
+
+  const updateName = async (name: string) => {
+    const publicKey = await getPublicKey();
+    if (!publicKey) return;
+    const res = await db
+      .collection("User")
+      .record(publicKey)
+      .call("setName", [name]);
+  };
+
+  useEffect(() => {
+    if (!auth) return;
+
+    auth.onAuthUpdate((authState) => {
+      if (!authState) return;
+
+      setAuthed(!!authState);
+
+      db.signer(async (data) => {
+        return {
+          h: "eth-personal-sign",
+          sig: await auth.ethPersonalSign(data),
+        };
+      });
+    });
+  }, []);
+
+  return {
+    authed,
+    signIn,
+    signOut,
+    updateName,
+  };
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,40 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import { toast } from "sonner";
+import { Auth } from "@polybase/auth";
+import {
+  ethPersonalSign,
+  ethPersonalSignRecoverPublicKey,
+} from "@polybase/eth";
+import { Polybase } from "@polybase/client";
+import { useEffect, useState } from "react";
+import { useCollectionUser, usePB } from "@/libs/hooks/use-polybase";
 
 export default function Home() {
+  const [publicKey, setPublicKey] = useState<string | null>(null);
+  const [nftId, setNftId] = useState("");
+
+  const { data, error, loading } = useCollectionUser();
+  console.log("🚀 ~ file: index.tsx:17 ~ Home ~ data:", data);
+  const { authed, signIn, signOut, updateName } = usePB();
+
   return (
     <>
       <div>hello world</div>
       <Typography variant="h2">Lorem Ipsum</Typography>
-      <Button onClick={() => toast('hello')} variant="contained">Hello World</Button>
+
+      <Button onClick={() => updateName("hello")} variant="contained">
+        Update record
+      </Button>
+
+      {authed ? (
+        <Button onClick={signOut} variant="contained">
+          Logout
+        </Button>
+      ) : (
+        <Button onClick={signIn} variant="contained">
+          Login
+        </Button>
+      )}
     </>
   );
 }
