3d7a2347ca2a1fa0ace76e275eee93faf41ea563
aeither <36173828+aeither@users.noreply.github.com>
2023-03-15 11:23:13 +0100

    mui x tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { Button, Typography } from "@mui/material";
+import { toast } from "sonner";
 
 export default function Home() {
   return (
     <>
       <div>hello world</div>
       <Typography variant="h2">Lorem Ipsum</Typography>
-      <Button variant="contained">Hello World</Button>
+      <Button onClick={() => toast('hello')} variant="contained">Hello World</Button>
     </>
   );
 }
