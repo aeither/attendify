6b10ea87cd6dbd0cbff9cd4375bcc83541810735
aeither <36173828+aeither@users.noreply.github.com>
2023-03-16 21:25:39 +0100

    add schema

---
+++ polybase/schema.ts
@@ -0,0 +1,59 @@
+import { Polybase } from '@polybase/client'
+export const applySchema = async () => {
+  const db = new Polybase({
+    defaultNamespace: 'attendify',
+  })
+
+  return await db.applySchema(
+    `
+    @public
+    collection User {
+      id: string;
+      publicKey: PublicKey;
+      name?: string; 
+      
+      image?: string;
+      level?: number;
+      points?: number;
+    
+      constructor (id: string) {
+        this.id = id;
+        
+        this.publicKey = ctx.publicKey;
+        this.name = 'anonymous';
+            this.points = 0;
+      }
+    
+      function setName (name: string) {
+        if (ctx.publicKey != this.publicKey) {
+          error('You are not the creator of this record.');
+        }
+        this.name = name;
+      }
+    
+      function setPoints (points: number) {
+        if (ctx.publicKey != '0x000000000') {
+          error('You are not allowed to update the points.');
+        }
+        this.points = points;
+      }
+    
+      @index(id);
+    }
+    
+    @public
+    collection Event {
+      id: string;
+      description: string;
+      date: number;
+      location: string;
+      participants: string[];
+    
+      constructor () {
+     
+      }
+    }    
+`,
+    'attendify',
+  ) // your-namespace is optional if you have defined a default namespace
+}
