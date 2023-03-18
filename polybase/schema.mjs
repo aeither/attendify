ca9c89f0b2acde5e0016b605f30a68966880a0bf
aeither <36173828+aeither@users.noreply.github.com>
2023-03-18 10:16:07 +0100

    upda

--- polybase/schema.mjs
+++ polybase/schema.mjs
@@ -44,13 +44,15 @@ collection User {
 @public
 collection Event {
   id: string;
+  title: string;
   description: string;
   date: number;
   location: string;
   participants: string[];
 
-  constructor (id: string, description: string, date: string, location: string, participant: string) {
+  constructor (id: string, title: string, description: string, date: string, location: string, participant: string) {
     this.id = id;
+    this.title = title;
     this.description = description;
     this.date = date;
     this.location = location;
